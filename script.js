63090f3ccb8877f5ad00b5314385acacffc158aa
Tomasz Kowalczyk <kowalfree@gmail.com>
2023-04-16 18:48:25 +0200

    Zmieniono paski nawigacji i dodano animację do okregów z cenami

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const text = document.querySelector(".text");
 const btn = document.querySelector(".send");
 
 const check = (input) => {
-	if (name.value === "" || mail.value === "" || text.value === "") {
-		alert("NIE WYSŁANO. WYPEŁNIJ WSZYSTKIE POLA!");
-	}
-
 	input.forEach((el) => {
 		if (el.value === "") {
 			el.style.backgroundColor = "#fcdddffa";
@@ -20,8 +16,8 @@ const check = (input) => {
 
 const length = () => {
 	if (
-		(name.value.length < 3 && name.value !== "") ||
-		(text.value.length < 15 && text.value !== "")
+		(0 < name.value.length < 3 && name.value !== "") ||
+		(0 < text.value.length < 15 && text.value !== "")
 	) {
 		alert(`WPISAŁEŚ ZA MAŁĄ ILOŚĆ ZNAKÓW!`);
 	}
@@ -35,34 +31,38 @@ const length = () => {
 	error(name, 3);
 	error(text, 15);
 };
+const checkMail = () => {
+	const regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,})$/;
 
-btn.addEventListener("click", (e) => {
-	e.preventDefault();
+	if (regex.test(mail.value)) {
+		if (text.value.length >= 15 && name.value.length >= 3) {
+			const body = `Student: ${name.value} <br >
 
-	const checkMail = () => {
-		const regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,})$/;
+		My e-mail: ${mail.value} <br > ${text.value}`;
 
-		if (regex.test(mail.value)) {
-			if (text.value.length >= 15) {
-				const body = `Student: ${name.value} <br >
-
-	        My e-mail: ${mail.value} <br > ${text.value}`;
-
-				Email.send({
-					Host: "smtp.elasticemail.com",
-					Username: "kowalfree@op.pl",
-					Password: "269AF8EF35CD2DFCF63D7E1F3256A84C4E7F",
-					To: "kowalfree@op.pl",
-					From: "kowalfree@op.pl",
-					Subject: "Chinese Language",
-					Body: body,
-				}).then((message) => alert("WYSŁANO WIADOMOŚĆ"));
-			}
-		} else {
+			Email.send({
+				Host: "smtp.elasticemail.com",
+				Username: "yourname",
+				Password: "yourpassword",
+				To: "your email",
+				From: "your email",
+				Subject: "Chinese Language",
+				Body: body,
+			}).then((message) => alert("WYSŁANO WIADOMOŚĆ"));
+		}
+	} else {
+		if (name.value === "" || mail.value === "" || text.value === "") {
+			alert("NIE WYSŁANO. WYPEŁNIJ WSZYSTKIE POLA!");
+		}
+		if (mail.value !== "") {
 			mail.style.backgroundColor = "#fcdddffa";
 			alert(`EMAIL JEST NIEPOPRAWNY!`);
 		}
-	};
+	}
+};
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault();
 
 	check([name, mail, text]);
 	length();
